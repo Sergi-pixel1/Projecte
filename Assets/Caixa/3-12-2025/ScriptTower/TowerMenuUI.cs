053fe0c72335765c5ad2df8e36f39c419f2c4d0f
Sergi-pixel1 <segoruu@gmail.com>
2025-12-16 17:38:50 +0100

    abanspregunta

---
+++ Assets/Caixa/3-12-2025/ScriptTower/TowerMenuUI.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class TowerMenuUI : MonoBehaviour
+{
+    public void BuildTower1()
+    {
+        TowerManager.instance.BuildTower1();
+    }
+
+    public void BuildTower2()
+    {
+        TowerManager.instance.BuildTower2();
+    }
+}
+
